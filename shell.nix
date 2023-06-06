6f92a3b54506ed91c8c1cb4e47748a65774873d4
Darwin D Wu <darwin67@users.noreply.github.com>
2023-06-06 12:49:21 -0700

    Add nix shell for local development setup

---
+++ shell.nix
@@ -0,0 +1,21 @@
+{ pkgs ? import (fetchTarball
+  "https://github.com/NixOS/nixpkgs/archive/refs/tags/23.05.tar.gz") { } }:
+
+with pkgs;
+
+mkShell {
+  buildInputs = [
+    # Elixir
+    pkgs.elixir
+    pkgs.erlang
+
+    # LSPs
+    pkgs.elixir-ls
+    pkgs.erlang-ls
+
+    # Tools
+    pkgs.yamllint
+    pkgs.shfmt
+    pkgs.shellcheck
+  ];
+}
