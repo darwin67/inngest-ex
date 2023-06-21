6379474ddb6ad278d973051029e30fae62b86d93
Darwin D Wu <darwin67@users.noreply.github.com>
2023-06-20 23:11:31 -0700

    Add event parsing from map data

--- lib/inngest/event.ex
+++ lib/inngest/event.ex
@@ -6,6 +6,8 @@ defmodule Inngest.Event do
   defstruct [
     :name,
     :data,
+    :ts,
+    :datetime,
     id: "",
     user: %{}
   ]
@@ -14,8 +16,25 @@ defmodule Inngest.Event do
           id: binary(),
           name: binary(),
           data: any(),
-          user: map()
+          user: map(),
+          ts: number(),
+          datetime: DateTime.t()
         }
+
+  def from(data) do
+    newmap =
+      for {key, val} <- data, into: %{} do
+        {String.to_existing_atom(key), val}
+      end
+
+    newmap =
+      case newmap |> Map.get(:ts, 0) |> DateTime.from_unix(:millisecond) do
+        {:ok, datetime} -> newmap |> Map.put(:datetime, datetime)
+        _ -> newmap
+      end
+
+    struct(__MODULE__, newmap)
+  end
 end
 
 defimpl Jason.Encoder, for: Inngest.Event do
