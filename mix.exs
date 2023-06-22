e33f9a04221d9ec3f56a5611ec870eab319ae9b7
Darwin D Wu <darwin67@users.noreply.github.com>
2023-06-21 22:08:56 -0700

    deps cleanup

--- mix.exs
+++ mix.exs
@@ -60,11 +60,6 @@ defmodule Inngest.MixProject do
       # {:bandit, "~> 0.6"},
 
       # dev / test
-      {:phoenix, "~> 1.6", only: :dev},
-      {:phoenix_html, "~> 3.3"},
-      {:phoenix_live_view, "~> 0.19", only: :dev},
-      {:phoenix_live_reload, "~> 1.2", only: :dev},
-      {:tailwind, "~> 0.2.0", runtime: Mix.env() == :dev},
       {:ex_doc, "~> 0.27", only: :dev, runtime: false},
       {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
       {:dialyxir, "~> 1.0", only: [:dev], runtime: false}
@@ -76,7 +71,8 @@ defmodule Inngest.MixProject do
       lint: ["credo"],
       "fmt:check": [
         "format --check-formatted mix.exs 'lib/**/*.{ex,exs}' 'test/**/*.{ex,exs}'"
-      ]
+      ],
+      dev: "run --no-halt dev.exs"
     ]
   end
 end
