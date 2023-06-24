df96f0c0e336de5ad557fe7b60d874d149e1b2da
Darwin D Wu <darwin67@users.noreply.github.com>
2023-06-23 23:52:08 -0700

    remove struct definition and experiment with run syntax

--- lib/inngest/function/step.ex
+++ lib/inngest/function/step.ex
@@ -1,14 +1,6 @@
 defmodule Inngest.Function.Step do
   @moduledoc false
 
-  defstruct [
-    :id,
-    :name,
-    :path,
-    :retries,
-    :runtime
-  ]
-
-  def run(name, do: block) do
+  def run(ctx, name, do: block) do
   end
 end
