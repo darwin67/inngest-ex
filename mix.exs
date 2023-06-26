7a4e9b66d80486163cd3e8104a3c7b2cd633e074
Darwin D Wu <darwin67@users.noreply.github.com>
2023-06-26 12:39:41 +0200

    split out some dev modules

--- mix.exs
+++ mix.exs
@@ -22,6 +22,7 @@ defmodule Inngest.MixProject do
     ]
   end
 
+  defp elixirc_paths(:dev), do: ["lib", "dev"]
   defp elixirc_paths(:test), do: ["lib", "test/support"]
   defp elixirc_paths(_), do: ["lib"]
 
