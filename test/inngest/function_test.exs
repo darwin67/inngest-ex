c6682fabc4b7d5e6782b952ec1d40397aba9b340
Darwin D Wu <darwin67@users.noreply.github.com>
2023-07-11 01:26:10 -0700

    Add datetime format

--- test/inngest/function_test.exs
+++ test/inngest/function_test.exs
@@ -1,6 +1,7 @@
 defmodule Inngest.FunctionTest do
   use ExUnit.Case, async: true
 
+  alias Inngest.Function
   alias Inngest.Function.Trigger
   alias Inngest.{TestEventFn, TestCronFn}
 
@@ -70,6 +71,39 @@ defmodule Inngest.FunctionTest do
              } = TestEventFn.__handler__()
 
       assert Enum.count(steps) == 8
+    end
+  end
+
+  describe "validate_datetime/1" do
+    @formats [
+      # RFC3339
+      "2013-03-05T23:25:19+02:00",
+      # RFC3339z
+      "2013-03-05T23:25:19Z",
+      # RFC1123
+      "Tue, 05 Mar 2013 23:25:19 +0200",
+      # RFC822
+      "Mon, 05 Jun 14 23:20:59 UT",
+      # RFC822z
+      "Mon, 05 Jun 14 23:20:59 Z",
+      # Custom
+      "Monday, 02-Jan-06 15:04:05 MST",
+      "Mon Jan 02 15:04:05 -0700 2006",
+      # UNIX
+      "Tue Mar 5 23:25:19 PST 2013",
+      # ANSIC
+      "Tue Mar 5 23:25:19 2013",
+      # Timestamp, second & millisecond
+      "Jan _2 15:04:05",
+      "Jan _2 15:04:05.000",
+      # ISOdate
+      "2006-01-02"
+    ]
+
+    for fmt <- @formats do
+      test "should be able to parse '#{fmt}'" do
+        assert _ = Function.validate_datetime(fmt)
+      end
     end
   end
 end
