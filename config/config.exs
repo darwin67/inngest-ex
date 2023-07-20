6028f8bbb8942427d8c8889bc28ed3233fbf5d40
Darwin D Wu <darwin67@users.noreply.github.com>
2023-07-20 00:36:14 -0700

    Add test signing key

--- config/config.exs
+++ config/config.exs
@@ -6,4 +6,5 @@ config :elixir, :time_zone_database, Tz.TimeZoneDatabase
 config :tesla, adapter: {Tesla.Adapter.Hackney, [recv_timeout: 10_000]}
 
 config :inngest,
-  env: :dev
+  env: :dev,
+  signing_key: "signkey-test-8ee2262a15e8d3c42d6a840db7af3de2aab08ef632b32a37a687f24b34dba3ff"
