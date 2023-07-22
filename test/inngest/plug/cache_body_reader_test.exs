03b8be0f2f407288fc2d9407adaf87b7fc507a13
Darwin D Wu <darwin67@users.noreply.github.com>
2023-07-21 23:58:48 -0700

    Add plug for caching body reader

---
+++ test/inngest/plug/cache_body_reader_test.exs
@@ -0,0 +1,28 @@
+defmodule Inngest.Plug.CacheBodyReaderTest do
+  use ExUnit.Case, async: true
+  use Plug.Test
+
+  alias Inngest.Plug.CacheBodyReader
+
+  describe "read_body/2" do
+    @body %{"hello" => "world"}
+
+    setup do
+      conn =
+        conn(:post, "/api/inngest", Jason.encode!(@body))
+        |> Plug.Conn.put_req_header("content-type", "application/json")
+
+      %{conn: conn}
+    end
+
+    test "should cache body in assigns", %{conn: conn} do
+      assert {:ok, _body,
+              %{
+                assigns: %{raw_body: [raw]}
+              }} = CacheBodyReader.read_body(conn, [])
+
+      assert {:ok, body} = Jason.decode(raw)
+      assert body == @body
+    end
+  end
+end
