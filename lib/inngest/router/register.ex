20ed86258ac7befc168ea595e82b7b73030862f1
Darwin D Wu <darwin67@users.noreply.github.com>
2023-07-22 17:09:18 -0700

    Move registration function into register route module

--- lib/inngest/router/register.ex
+++ lib/inngest/router/register.ex
@@ -4,20 +4,26 @@ defmodule Inngest.Router.Register do
   """
   import Plug.Conn
   import Inngest.Router.Helper
+  alias Inngest.Config
 
   @content_type "application/json"
 
+  defdelegate httpclient(type, opts), to: Inngest.Client
+
+  @spec init(map()) :: map()
   def init(opts), do: opts
+
+  @spec call(Plug.Conn.t(), map()) :: Plug.Conn.t()
   def call(conn, opts), do: exec(conn, opts)
 
   defp exec(
          %{request_path: path} = conn,
-         %{funcs: funcs} = _params
+         %{funcs: funcs, framework: framework} = _params
        ) do
     funcs = func_map(path, funcs)
 
     {status, resp} =
-      case Inngest.Client.register(path, funcs) do
+      case register(path, funcs, framework: framework) do
         :ok ->
           {200, %{}}
 
@@ -34,5 +40,44 @@ defmodule Inngest.Router.Register do
     conn
     |> put_resp_content_type(@content_type)
     |> send_resp(status, resp)
+  end
+
+  defp register(path, functions, opts \\ []) do
+    framework = Keyword.get(opts, :framework)
+
+    payload = %{
+      url: Config.app_host() <> path,
+      v: "1",
+      deployType: "ping",
+      sdk: Config.sdk_version(),
+      framework: framework,
+      appName: Config.app_name(),
+      functions: functions |> Enum.map(fn {_, v} -> v.mod.serve(path) end)
+    }
+
+    key = Inngest.Signature.hashed_signing_key(Config.signing_key())
+    headers = if is_nil(key), do: [], else: [authorization: "Bearer " <> key]
+
+    headers =
+      if is_nil(Config.inngest_env()),
+        do: headers,
+        else: Keyword.put(headers, :"x-inngest-env", Config.inngest_env())
+
+    case Tesla.post(httpclient(:register, headers: headers), "/fn/register", payload) do
+      {:ok, %Tesla.Env{status: 200}} ->
+        :ok
+
+      {:ok, %Tesla.Env{status: 201}} ->
+        :ok
+
+      {:ok, %Tesla.Env{status: 202}} ->
+        :ok
+
+      {:ok, %Tesla.Env{status: _, body: error}} ->
+        {:error, error}
+
+      {:error, error} ->
+        {:error, error}
+    end
   end
 end
