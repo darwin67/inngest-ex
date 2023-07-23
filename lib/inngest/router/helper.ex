9197fae4be31d9fb230719a9b7ce385178f51099
Darwin D Wu <darwin67@users.noreply.github.com>
2023-07-23 12:03:41 -0700

    Ignore code compilation warnings when loading functions

--- lib/inngest/router/helper.ex
+++ lib/inngest/router/helper.ex
@@ -2,6 +2,7 @@ defmodule Inngest.Router.Helper do
   @moduledoc """
   Helper module for router and plugs
   """
+  @conflict_opt :ignore_module_conflict
 
   @spec func_map(binary(), list()) :: map()
   def func_map(path, funcs) do
@@ -12,7 +13,11 @@ defmodule Inngest.Router.Helper do
     end)
   end
 
+  @spec load_functions_from_path(map()) :: map()
   def load_functions_from_path(%{path: paths} = kv) when is_list(paths) do
+    ignored = Code.get_compiler_option(@conflict_opt)
+    unless ignored, do: Code.put_compiler_option(@conflict_opt, true)
+
     {:ok, modules, _warnings} =
       paths
       |> Enum.map(&Path.wildcard/1)
@@ -21,16 +26,23 @@ defmodule Inngest.Router.Helper do
       |> Enum.uniq()
       |> Kernel.ParallelCompiler.compile()
 
+    unless ignored, do: :ok = Code.put_compiler_option(@conflict_opt, false)
+
     funcs = Map.get(kv, :funcs, [])
     Map.put(kv, :funcs, funcs ++ modules)
   end
 
   def load_functions_from_path(%{path: path} = kv) when is_binary(path) do
+    ignored = Code.get_compiler_option(@conflict_opt)
+    unless ignored, do: Code.put_compiler_option(@conflict_opt, true)
+
     {:ok, modules, _warnings} =
       path
       |> Path.wildcard()
       |> Enum.filter(&(!File.dir?(&1)))
       |> Kernel.ParallelCompiler.compile()
+
+    unless ignored, do: :ok = Code.put_compiler_option(@conflict_opt, false)
 
     funcs = Map.get(kv, :funcs, [])
     Map.put(kv, :funcs, funcs ++ modules)
