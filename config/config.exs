5f65b158034000242ce824fe96926194c3d5f540
Darwin D Wu <darwin67@users.noreply.github.com>
2023-08-05 23:17:50 -0700

    add config template as comments

--- config/config.exs
+++ config/config.exs
@@ -6,5 +6,8 @@ config :elixir, :time_zone_database, Tz.TimeZoneDatabase
 config :tesla, adapter: {Tesla.Adapter.Hackney, [recv_timeout: 10_000]}
 
 config :inngest,
-  env: :dev,
-  path_runtime_eval: true
+  # app_host: "https://79c9-99-123-3-121.ngrok-free.app",
+  # path_runtime_eval: true,
+  # signing_key: "",
+  # event_key: ""
+  env: :dev
