31517460c5f23d30fd126657ff5a2929bc9c40ac
Darwin D Wu <darwin67@users.noreply.github.com>
2023-11-23 12:44:00 -0800

    Add more headers

--- lib/inngest/headers.ex
+++ lib/inngest/headers.ex
@@ -3,10 +3,17 @@ defmodule Inngest.Headers do
   Header values used by the SDK
   """
 
+  def env, do: "X-Inngest-Env"
   def forwarded_for, do: "X-Forwarded-For"
   def framework, do: "X-Inngest-Framework"
-  def no_retry, do: "X-Inngest-No-Retry"
+  def platform, do: "X-Inngest-Platform"
   def sdk, do: "X-Inngest-SDK"
+  def signature, do: "X-Inngest-Signature"
   def server_kind, do: "X-Inngest-Server-Kind"
+  def req_version, do: "X-Inngest-Req-Version"
   def server_timing, do: "Server-Timing"
+
+  # Retries
+  def no_retry, do: "X-Inngest-No-Retry"
+  def retry_after, do: "Retry-After"
 end
