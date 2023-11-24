cdeff44ab611e36cc8f130647fca97ddc7105e0d
Darwin D Wu <darwin67@users.noreply.github.com>
2023-11-23 18:20:22 -0800

    remove redundant call to serve

--- lib/inngest/router/register.ex
+++ lib/inngest/router/register.ex
@@ -22,7 +22,9 @@ defmodule Inngest.Router.Register do
     funcs =
       params
       |> load_functions()
-      |> func_map(path)
+      |> Enum.map(fn func ->
+        func.serve(path)
+      end)
 
     {status, resp} =
       case register(path, funcs, framework: framework) do
@@ -54,7 +56,7 @@ defmodule Inngest.Router.Register do
       sdk: Config.sdk_version(),
       framework: framework,
       appName: Config.app_name(),
-      functions: functions |> Enum.map(fn {_, v} -> v.mod.serve(path) end)
+      functions: functions
     }
 
     key = Inngest.Signature.hashed_signing_key(Config.signing_key())
