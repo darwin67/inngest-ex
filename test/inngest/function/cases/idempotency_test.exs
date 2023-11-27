dd6509681f94fc12cf6809793922aa33788aed56
Darwin D Wu <darwin67@users.noreply.github.com>
2023-11-26 21:23:57 -0800

    expand test

--- test/inngest/function/cases/idempotency_test.exs
+++ test/inngest/function/cases/idempotency_test.exs
@@ -8,7 +8,8 @@ defmodule Inngest.Function.Cases.IdempotencyTest do
 
   @tag :integration
   test "should only run 1 out of 10" do
-    event_ids = Enum.map(1..10, fn _ -> send_test_event("test/plug.idempotency") end)
+    event_ids =
+      Enum.map(1..10, fn _ -> send_test_event("test/plug.idempotency", %{foobar: false}) end)
 
     Process.sleep(@default_sleep)
 
@@ -34,5 +35,19 @@ defmodule Inngest.Function.Cases.IdempotencyTest do
       |> Enum.filter(&(!is_nil(&1)))
 
     assert Enum.count(fn_runs) == 1
+
+    # sending with a different value will run
+    other = send_test_event("test/plug.idempotency", %{foobar: "hello"})
+    Process.sleep(@default_sleep)
+
+    assert {:ok,
+            %{
+              "data" => [
+                %{
+                  "output" => "Done",
+                  "status" => "Completed"
+                }
+              ]
+            }} = DevServer.run_ids(other)
   end
 end
