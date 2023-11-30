7a7fa775c67a0afa4bbcfa5b55332649462d0aff
Darwin D Wu <darwin67@users.noreply.github.com>
2023-11-29 20:23:58 -0800

    add test for event conversion

--- lib/inngest/event.ex
+++ lib/inngest/event.ex
@@ -47,7 +47,7 @@ defmodule Inngest.Event do
   @type t() :: %__MODULE__{
           id: binary() | nil,
           name: binary(),
-          data: any(),
+          data: map(),
           v: binary() | nil,
           ts: number() | nil,
           datetime: DateTime.t() | nil
@@ -57,20 +57,17 @@ defmodule Inngest.Event do
   Construct an Event struct from a map.
   """
   @spec from(map()) :: t()
-  def from(data) do
+  def from(%{} = data) do
     newmap =
-      for {key, val} <- data, into: %{} do
-        {String.to_existing_atom(key), val}
-      end
-
-    newmap =
-      case newmap |> Map.get(:ts, 0) |> DateTime.from_unix(:millisecond) do
-        {:ok, datetime} -> newmap |> Map.put(:datetime, datetime)
-        _ -> newmap
+      case data |> Map.get(:ts, 0) |> DateTime.from_unix(:millisecond) do
+        {:ok, datetime} -> data |> Map.put(:datetime, datetime)
+        _ -> data
       end
 
     struct(__MODULE__, newmap)
   end
+
+  def from(_), do: %__MODULE__{}
 end
 
 defimpl Jason.Encoder, for: Inngest.Event do
