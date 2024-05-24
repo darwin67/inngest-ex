3d0de7512d643b14cebd05a0b9370730847357d2
Darwin <5746693+darwin67@users.noreply.github.com>
2024-05-24 01:45:06 -0700

    chore: replace nix shell with flake (#81)

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "Elixir SDK for Inngest";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = import nixpkgs { inherit system; };
+      in {
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            elixir
+            erlang_27
+
+            # LSPs
+            elixir-ls
+            lexical
+            erlang-ls
+
+            # Tools
+            yamllint
+            shfmt
+            shellcheck
+            git-cliff
+          ];
+        };
+      });
+}
