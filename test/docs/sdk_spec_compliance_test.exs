44ae061c0d76ff198019d8f3b995f7a05ebecc3d
Darwin D Wu <darwin67@users.noreply.github.com>
2026-06-19 15:17:16 -0700

    update plan name and compliance doc

--- test/docs/sdk_spec_compliance_test.exs
+++ test/docs/sdk_spec_compliance_test.exs
@@ -3,25 +3,9 @@ defmodule Inngest.Docs.SdkSpecComplianceTest do
 
   @doc_path Path.expand("../../docs/sdk-spec-compliance.md", __DIR__)
 
-  test "local plan links resolve from the compliance checklist" do
-    doc_dir = Path.dirname(@doc_path)
+  test "compliance checklist does not link to local plan files" do
     markdown = File.read!(@doc_path)
 
-    links =
-      ~r/\[[^\]]+\]\((?!https?:\/\/)([^)#]+)(?:#[^)]+)?\)/
-      |> Regex.scan(markdown, capture: :all_but_first)
-      |> List.flatten()
-
-    assert links != []
-
-    missing_links =
-      Enum.reject(links, fn link ->
-        doc_dir
-        |> Path.join(link)
-        |> Path.expand()
-        |> File.exists?()
-      end)
-
-    assert missing_links == []
+    refute markdown =~ ~r/\]\(plans\//
   end
 end
